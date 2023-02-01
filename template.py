67b307f665659e60247bec0c55d291c434a044e4
adam-j-pearce <120463633+adam-j-pearce@users.noreply.github.com>
2023-02-01 22:18:25 +0000

    updates

--- template.py
+++ template.py
@@ -1,28 +1,6 @@
-import irsdk
-import time
+driver_car_id = ir['PlayerCarIdx']
 
-# this is our State class, with some helpful variables
-class State:
-    ir_connected = False
-
-# here we check if we are connected to iracing
-# so we can retrieve some data
-def check_iracing():
-    if state.ir_connected and not (ir.is_initialized and ir.is_connected):
-        state.ir_connected = False
-        # we are shutting down ir library (clearing all internal variables)
-        ir.shutdown()
-        print('irsdk disconnected')
-    elif not state.ir_connected and ir.startup() and ir.is_initialized and ir.is_connected:
-        state.ir_connected = True
-        print('irsdk connected')
-     
-# our main loop, where we retrieve data
-# and do something useful with it
-def loop():
-
-    driver_car_id = ir['PlayerCarIdx']
-
+def car():
     car_name = ir['DriverInfo']['Drivers'][driver_car_id]['CarScreenName']
     car_class = ir['DriverInfo']['Drivers'][driver_car_id]['CarClassShortName']
     car_num = ir['DriverInfo']['Drivers'][driver_car_id]['CarNumber'] 
@@ -30,7 +8,7 @@ def loop():
     weight_pen = ir['DriverInfo']['Drivers'][driver_car_id]['CarClassWeightPenalty']
     
     car_info={
-        "car": {
+        car: {
             "name":car_name,
             "class":car_class,
             "number":car_num,
@@ -39,6 +17,7 @@ def loop():
         }
     }    
         
+def driver():
     driver_name = ir['DriverInfo']['Drivers'][driver_car_id]['UserName']
     driver_id = ir['DriverInfo']['Drivers'][driver_car_id]['UserID']
     if ir['DriverInfo']['Drivers'][driver_car_id]['TeamName'] == 1:
@@ -58,6 +37,7 @@ def loop():
         }
     }
 
+def telemetry():
     throttle = ir['Throttle']
     brake = ir['Brake']
     clutch = ir['Clutch']
@@ -101,6 +81,7 @@ def loop():
         
     }
 
+def session():
     session_type = ir['WeekendInfo']['EventType']
     session_id = ir['WeekendInfo']['SessionID']
     subsession_id = ir['WeekendInfo']['SubSessionID']
@@ -117,6 +98,7 @@ def loop():
         }
     }
 
+def track():
     track_name = ir['WeekendInfo']['TrackDisplayName']
     track_config = ir['WeekendInfo']['TrackConfigName']
 
@@ -127,26 +109,3 @@ def loop():
         }
     }
 
-    print(session_info|track_info|driver_info|car_info|telemetry)
-
-
-if __name__ == '__main__':
-    # initializing ir and state
-    ir = irsdk.IRSDK()
-    state = State()
-
-    try:
-        # infinite loop
-        while True:
-            # check if we are connected to iracing
-            check_iracing()
-            # if we are, then process data
-            if state.ir_connected:
-                loop()
-            # sleep for 1 second
-            # maximum you can use is 1/60
-            # cause iracing updates data with 60 fps
-            time.sleep(1)
-    except KeyboardInterrupt:
-        # press ctrl+c to exit
-        pass
